9f333048482bd3cdecca05fa831aabb895f4b976
Furkan Kuyumcu <fkuyumcu@k1m12s05.42istanbul.com.tr>
2024-10-22 18:05:20 +0300

    prints

--- ft_printf.c
+++ ft_printf.c
@@ -2,10 +2,65 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int printc(char c)
+{
+	write(1,&c,1);
+	return (1);
+}
 
+int prints(char *s)
+{
+	int n;
 
+	n = 0;
+	while(s)
+	{
+		write(1,s,1);
+		s++;
+		n++;
+	}
+	return (n);
+}
 
+int printp(int d)
+{
 
+}
+
+int printd(int d)
+{
+	
+}
+
+int printi(int d)
+{
+
+}
+
+int printu(unsigned int d)
+{
+
+}
+
+int printx(int x)
+{
+
+}
+
+int printx_(int x)
+{
+
+}
+
+int printper(char c)
+{
+
+}
+
+int printu(int p)
+{
+
+}
 
 int ft_print_format(char c,va_list ap)
 {
@@ -15,27 +70,29 @@ int ft_print_format(char c,va_list ap)
 	if (c == 'c')
 		n += printc(va_arg(ap,char));
 	else if (c == 's')
-		n += prints(va_arg(ap,char));
+		n += prints(va_arg(ap,char *));
 	else if (c == 'p')
 		n += printp(va_arg(ap,void*));
 	else if (c == 'd')
-		n += printp(va_arg(ap,void*));
+		n += printd(va_arg(ap,void*));
 	else if( c == 'i')
-		n += printp(va_arg(ap,void*));
+		n += printi(va_arg(ap,void*));
 	else if (c == 'u')
-		n += printp(va_arg(ap,void*));
+		n += printu(va_arg(ap,void*));
 	else if (c == 'x')
-		n += printp(va_arg(ap,void*));
+		n += printx(va_arg(ap,void*));
 	else if(c == 'X')
-		n += printp(va_arg(ap,void*));
+		n += printx_(va_arg(ap,void*));
 	else if(c == '%')
-		n += printp(va_arg(ap,void*));
+		n += printper(va_arg(ap,void*));
 
-
+	return (n);
 }
 
 int ft_printf(const char *s, ...)
 {
+	va_list ap;
+	va_start(ap,s);
 	int n;
 
 	n = 0;
